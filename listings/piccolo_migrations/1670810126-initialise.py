05df2e7398548a49973607b33d63f1ca464e6762
Ay'yaruq Dotharl <acchan.ff14@gmail.com>
2022-12-14 10:43:07 +1100

    Add initial migration

---
+++ listings/piccolo_migrations/1670810126-initialise.py
@@ -0,0 +1,496 @@
+from piccolo.apps.migrations.auto.migration_manager import MigrationManager
+from piccolo.columns.base import OnDelete
+from piccolo.columns.base import OnUpdate
+from piccolo.columns.column_types import Boolean
+from piccolo.columns.column_types import ForeignKey
+from piccolo.columns.column_types import Integer
+from piccolo.columns.column_types import JSONB
+from piccolo.columns.column_types import Timestamp
+from piccolo.columns.column_types import UUID
+from piccolo.columns.column_types import Varchar
+from piccolo.columns.defaults.timestamp import TimestampNow
+from piccolo.columns.defaults.uuid import UUID4
+from piccolo.columns.indexes import IndexMethod
+from piccolo.table import Table
+
+
+class Character(Table, tablename="character"):
+    content_id_sha256 = Varchar(
+        length=64,
+        default="",
+        null=False,
+        primary_key=True,
+        unique=True,
+        index=False,
+        index_method=IndexMethod.btree,
+        choices=None,
+        db_column_name=None,
+        secret=False,
+    )
+
+
+class Retainer(Table, tablename="retainer"):
+    content_id_sha256 = Varchar(
+        length=64,
+        default="",
+        null=True,
+        primary_key=True,
+        unique=True,
+        index=False,
+        index_method=IndexMethod.btree,
+        choices=None,
+        db_column_name=None,
+        secret=False,
+    )
+
+
+ID = "2022-12-12T12:55:26:391921"
+VERSION = "0.101.0"
+DESCRIPTION = "Initialise"
+
+
+async def forwards():
+    manager = MigrationManager(
+        migration_id=ID, app_name="listing", description=DESCRIPTION
+    )
+
+    manager.add_table("Listing", tablename="listing")
+
+    manager.add_table("Retainer", tablename="retainer")
+
+    manager.add_table("Character", tablename="character")
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="listing_id",
+        db_column_name="listing_id",
+        column_class_name="UUID",
+        column_class=UUID,
+        params={
+            "default": UUID4(),
+            "null": False,
+            "primary_key": True,
+            "unique": True,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="world_id",
+        db_column_name="world_id",
+        column_class_name="Integer",
+        column_class=Integer,
+        params={
+            "default": 0,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="item_id",
+        db_column_name="item_id",
+        column_class_name="Integer",
+        column_class=Integer,
+        params={
+            "default": 0,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="hq",
+        db_column_name="hq",
+        column_class_name="Boolean",
+        column_class=Boolean,
+        params={
+            "default": False,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="on_mannequin",
+        db_column_name="on_mannequin",
+        column_class_name="Boolean",
+        column_class=Boolean,
+        params={
+            "default": False,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="materia",
+        db_column_name="materia",
+        column_class_name="JSONB",
+        column_class=JSONB,
+        params={
+            "default": "{}",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="unit_price",
+        db_column_name="unit_price",
+        column_class_name="Integer",
+        column_class=Integer,
+        params={
+            "default": 0,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="quatity",
+        db_column_name="quatity",
+        column_class_name="Integer",
+        column_class=Integer,
+        params={
+            "default": 0,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="dye_id",
+        db_column_name="dye_id",
+        column_class_name="Integer",
+        column_class=Integer,
+        params={
+            "default": 0,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="last_review_time",
+        db_column_name="last_review_time",
+        column_class_name="Timestamp",
+        column_class=Timestamp,
+        params={
+            "default": TimestampNow(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="last_upload_time",
+        db_column_name="last_upload_time",
+        column_class_name="Timestamp",
+        column_class=Timestamp,
+        params={
+            "default": TimestampNow(),
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="creator",
+        db_column_name="creator",
+        column_class_name="ForeignKey",
+        column_class=ForeignKey,
+        params={
+            "references": Character,
+            "on_delete": OnDelete.cascade,
+            "on_update": OnUpdate.cascade,
+            "target_column": None,
+            "null": True,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="retainer",
+        db_column_name="retainer",
+        column_class_name="ForeignKey",
+        column_class=ForeignKey,
+        params={
+            "references": Retainer,
+            "on_delete": OnDelete.cascade,
+            "on_update": OnUpdate.cascade,
+            "target_column": None,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="seller",
+        db_column_name="seller",
+        column_class_name="ForeignKey",
+        column_class=ForeignKey,
+        params={
+            "references": Character,
+            "on_delete": OnDelete.cascade,
+            "on_update": OnUpdate.cascade,
+            "target_column": None,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Listing",
+        tablename="listing",
+        column_name="live",
+        db_column_name="live",
+        column_class_name="Boolean",
+        column_class=Boolean,
+        params={
+            "default": False,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Retainer",
+        tablename="retainer",
+        column_name="content_id_sha256",
+        db_column_name="content_id_sha256",
+        column_class_name="Varchar",
+        column_class=Varchar,
+        params={
+            "length": 64,
+            "default": "",
+            "null": True,
+            "primary_key": True,
+            "unique": True,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Retainer",
+        tablename="retainer",
+        column_name="world_id",
+        db_column_name="world_id",
+        column_class_name="Integer",
+        column_class=Integer,
+        params={
+            "default": 0,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Retainer",
+        tablename="retainer",
+        column_name="name",
+        db_column_name="name",
+        column_class_name="Varchar",
+        column_class=Varchar,
+        params={
+            "length": 20,
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Character",
+        tablename="character",
+        column_name="content_id_sha256",
+        db_column_name="content_id_sha256",
+        column_class_name="Varchar",
+        column_class=Varchar,
+        params={
+            "length": 64,
+            "default": "",
+            "null": False,
+            "primary_key": True,
+            "unique": True,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Character",
+        tablename="character",
+        column_name="world_id",
+        db_column_name="world_id",
+        column_class_name="Integer",
+        column_class=Integer,
+        params={
+            "default": 0,
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    manager.add_column(
+        table_class_name="Character",
+        tablename="character",
+        column_name="name",
+        db_column_name="name",
+        column_class_name="Varchar",
+        column_class=Varchar,
+        params={
+            "length": 20,
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+    )
+
+    return manager
